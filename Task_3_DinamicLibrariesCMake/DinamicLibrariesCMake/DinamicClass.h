4c95ad692a43cf335fa3e72db51976e27a3101b1
Scorpic <volkadav99@list.ru>
2023-04-10 21:56:45 +0300

    correct CMake

--- Task_3_DinamicLibrariesCMake/DinamicLibrariesCMake/DinamicClass.h
+++ Task_3_DinamicLibrariesCMake/DinamicLibrariesCMake/DinamicClass.h
@@ -1,14 +1,14 @@
 #pragma once
-#ifdef DINAMICLIBARIESCMAKE_EXPORTS
-#define DINAMICLIBARIESCMAKE_API __declspec(dllexport)
-#else
-#define DINAMICLIBARIESCMAKE_API __declspec(dllimport)
-#endif
+//#ifdef DINAMICLIBARIESCMAKE_EXPORTS
+//#define DINAMICLIBARIESCMAKE_API __declspec(dllexport)
+//#else
+//#define DINAMICLIBARIESCMAKE_API __declspec(dllimport)
+//#endif
 
 #include <iostream>
 
 class Greeter {
 public:
-	DINAMICLIBARIESCMAKE_API std::string greet(std::string name);
-	DINAMICLIBARIESCMAKE_API void EnterName();
+	std::string greet(std::string name);
+	void EnterName();
 };
